bbcff9a21b980e98aec167aa0b87371dc0955927
GK Hermes Agent <hermes@gkwebsites.co.uk>
2026-05-24 00:58:40 +0100

    Update supabase/migrations/20260523094907_656ca79e-4eb8-4cbd-9031-2458229d02a7.sql

--- supabase/migrations/20260523094907_656ca79e-4eb8-4cbd-9031-2458229d02a7.sql
+++ supabase/migrations/20260523094907_656ca79e-4eb8-4cbd-9031-2458229d02a7.sql
@@ -6,7 +6,7 @@ SELECT cron.schedule(
   $$
   SELECT net.http_post(
     url:='https://pnlocibettgyqyttegcu.supabase.co/functions/v1/heartbeat',
-    headers:='{"Content-Type":"application/json","apikey":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubG9jaWJldHRneXF5dHRlZ2N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyOTMyNTAsImV4cCI6MjA1Mjg2OTI1MH0.eA90Z5FYhTwVD37Lh7WX6ctYHSI4_Z_-q7oOL_4FkFg"}'::jsonb,
+    headers:='{"Content-Type":"application/json"}'::jsonb,
     body:=concat('{"time":"', now(), '"}')::jsonb
   ) as request_id;
   $$
